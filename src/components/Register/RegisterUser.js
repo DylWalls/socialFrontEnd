06ee0d5b9ee547ceb29fb3b50a73600f9d123e9e
asiaromaria <designsbyarc@gmail.com>
2021-09-09 13:36:32 -0700

    added input forms

--- src/components/Register/RegisterUser.js
+++ src/components/Register/RegisterUser.js
@@ -11,8 +11,11 @@ const Register = ({ createUser, deleteUser }) => {
     <div>
 
       <h2>Register Here</h2>
-      <form>
-        <input type="text" />
+      <form onSubmit={useState.handleSubmit}>
+        <input type="text" onChange={useState.handleChange}/>
+        <input type="text" value="create unique username"/>
+        <input type="text" value="create unique password including one number, one capital leter, one symbol, and at least 8 characters."/>
+      <input type="submit" value="Submit" />
       </form>
     </div>
   );
